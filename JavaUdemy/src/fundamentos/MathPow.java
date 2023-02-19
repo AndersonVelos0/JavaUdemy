c180e77c9e564821a283da84827731e4e41a60cb
Anderson <Anderson@DESKTOP-KAB4ON6>
2023-02-19 18:30:47 -0300

    Introduzindo operadores aritmeticos juntamente à função Math.pow para calcular potencia em Java

---
+++ JavaUdemy/src/fundamentos/MathPow.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class MathPow {
+
+	public static void main(String[] args) {
+		
+		int a = 2 + 3 * 4;
+		//PARA ELEVARMOS UM NUMERO A DETERMINADA POTENCIA USAMOS O JAVA.MATH.POW
+		int b = (int) Math.pow(a, 3);
+		
+		System.out.println(b );
+		
+	}
+	
+}
---
+++ JavaUdemy/src/fundamentos/OperadoresAritmeticos.java
@@ -0,0 +1,41 @@
+package fundamentos;
+
+public class OperadoresAritmeticos {
+	
+	public static void main (String [] args) {
+		
+		//pode-se efetuar calculos dentro do output diretamente
+		
+		System.out.println(2 + 3);
+		
+		double x = 34.56;
+		double y = 23.56;
+		
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println(x / y);
+		
+		int a = 8; 
+		int b = 156;
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a / (double) b);
+		
+		System.out.println(a % b);
+		System.out.println(8 %  156);
+
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
