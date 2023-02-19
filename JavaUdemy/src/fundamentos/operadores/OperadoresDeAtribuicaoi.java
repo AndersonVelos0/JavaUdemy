a230963365e67ae79781f3f8626106b7cf85ef91
Anderson <Anderson@DESKTOP-KAB4ON6>
2023-02-19 19:30:31 -0300

    Adicionando os operadores de atribuição ao conteudo

---
+++ JavaUdemy/src/fundamentos/operadores/OperadoresDeAtribuicaoi.java
@@ -0,0 +1,21 @@
+package fundamentos.operadores;
+
+public class OperadoresDeAtribuicaoi {
+	
+	public static void main(String[] args) {
+		
+		int a = 3; 
+		int b = a;
+		int c = a + b;
+		
+		
+		c += b; // c = c + b
+		c -= b; // c = c - b
+		c *= b; // c = c * b
+		c /= b; // c = c / b
+		c %= b; // c = c % b se der 0 é par, se der 1 é impar
+		
+		
+	}
+
+}
