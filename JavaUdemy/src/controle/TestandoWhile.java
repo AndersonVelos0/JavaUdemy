1d5342080e705f62970bb0aa6ab3f3da6235b9a5
Anderson <Anderson@DESKTOP-KAB4ON6>
2023-03-02 00:20:34 -0300

    Introduzindo o while ao aprendizado, a variavel é utilizada para incontaveis vezes.

---
+++ JavaUdemy/src/controle/TestandoWhile.java
@@ -0,0 +1,19 @@
+package controle;
+
+public class TestandoWhile {
+	
+	public static void main(String[] args) {
+		//While determinado
+		int bomDia =  1;
+		
+		//While irá retornar verdadeiro ou falso 
+		//Trata-se de uma variavel de controle que permite efetuar atribuições com indeterminadas vezes
+		
+		while (bomDia <= 10) {
+			System.out.println("Bom dia!");
+			bomDia++;
+		}
+		
+	}
+
+}
