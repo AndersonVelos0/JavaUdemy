71b1eea00f9b4312ac4d5343cd0388b0a4558431
Anderson <Anderson@DESKTOP-KAB4ON6>
2023-03-02 00:28:19 -0300

    Adicionando o for aos estudos

---
+++ JavaUdemy/src/controle/TestandoFor.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class TestandoFor {
+
+	public static void main(String[] args) {
+		//for é utilizada para contagens limitadas
+	
+		for(int contador = 0; contador <= 20; contador +=2) {
+			System.out.printf("i = %d\n", contador);	
+		
+		}
+		
+		
+	}
+}
