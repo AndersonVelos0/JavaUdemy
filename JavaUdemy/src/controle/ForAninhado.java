a8b0c536ef9cb44af755c06ff441638dd67f578b
Anderson <Anderson@DESKTOP-KAB4ON6>
2023-03-07 13:27:07 -0300

    Adicionando for aos estudos, reforçando conceitos básicos do java.

---
+++ JavaUdemy/src/controle/ForAninhado.java
@@ -0,0 +1,20 @@
+package controle;
+
+public class ForAninhado {
+
+	public static void main(String[] args) {
+		
+		//
+		for (int i = 0; i < 10; i++) {
+			//AO CHAMARMOS OUTRO FOR, ELE IRÁ REPETIR O LAÇO MAIS 10 vezes, resultando em 100x repetidas
+			//executando no modo debbug, clicando no inseto, podemos ver o laço de repetição e sua entrada
+			for(int j = 0; j < 10; j++) {
+				System.out.printf("[%d %d]", i, j);
+			}
+		
+				System.out.println();
+		}
+		
+	}
+
+}
---
+++ JavaUdemy/src/controle/ForDecrescente.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class ForDecrescente {
+
+	public static void main(String[] args) {
+		
+		for (int cont = 10; cont >= 0; cont -=2) {
+			
+			System.out.printf("Contador = %d\n ", cont);
+		}
+		
+		
+	}
+}
