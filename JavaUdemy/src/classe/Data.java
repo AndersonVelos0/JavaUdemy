02e0b9148bfda7357e90b1e9272b4cae5274268e
Anderson <Anderson@DESKTOP-KAB4ON6>
2023-03-12 13:39:24 -0300

    Criando os construtores para passagem das datas, passados os parametros na classe DataTeste.

--- JavaUdemy/src/classe/Data.java
+++ JavaUdemy/src/classe/Data.java
@@ -6,6 +6,22 @@ public class Data {
 	int month; 
 	int year;
 	
+	//Adicionado construtor padrão na classe Data
+	Data(){
+		day = 1;
+		month = 1; 
+		year = 1970;
+
+	}
+	//Construtor passando parametros na classe data
+	Data(int day1, int month1, int year1){
+		day = day1;
+		month = month1; 
+		year = year1;
+		
+		System.out.printf("%d/%d/%d", day1, month1, year1);
+		
+	}
 	//adicionando o método para mostrar a data
 	String showDate() {
 		return String.format("%d/%d/%d", day, month, year);
--- JavaUdemy/src/classe/DataTeste.java
+++ JavaUdemy/src/classe/DataTeste.java
@@ -3,16 +3,17 @@ package classe;
 public class DataTeste {
 
 	public static void main(String[] args) {
-
+		//Após a criação dos construtores, não será necessário criar as variaveis do objeto, pois 
+		//o parametro já foi passado.1'
 		Data date1 = new Data();
-		date1.day = 28;
-		date1.month = 12;
-		date1.year = 2016;
+		Data date2 = new Data(28, 12, 2018);
 
-		System.out.println(date1.day + "/" + date1.month + "/" + date1.year);
-		//adicionando o método showDate para mostrar a data completa através da função
-		System.out.printf(date1.showDate());
-		
+		// System.out.println(date1.day + "/" + date1.month + "/" + date1.year);
+
+		// adicionando o método showDate para mostrar a data completa através da função
+		System.out.println("\n" + date1.showDate());
+
+
 	}
 
 }
