1e88e1d93c89a2ed64f3ed3cf6d76e0f9893685a
Anderson <Anderson@DESKTOP-KAB4ON6>
2023-03-13 00:57:23 -0300

    Postando atualizações utilizando this. e this(), um como método e outro como referenciação das variaveis de instancia.

--- JavaUdemy/src/classe/Data.java
+++ JavaUdemy/src/classe/Data.java
@@ -1,29 +1,44 @@
 package classe;
 
 public class Data {
-	
-	int day; 
-	int month; 
-	int year;
-	
-	//Adicionado construtor padrão na classe Data
-	Data(){
-		day = 1;
-		month = 1; 
-		year = 1970;
 
+	int day;
+	int month;
+	int year;
+
+	// Adicionado construtor padrão na classe Data
+	Data() {
+		//day = 1;
+		//month = 1;
+		//year = 1970;
+
+		//Utilizando o diz dentro do construtor como função/método
+		//Sempre que usado o this como método você está chamando um construtor a partir de outro
+		this(1,1,1970);
 	}
-	//Construtor passando parametros na classe data
-	Data(int day1, int month1, int year1){
-		day = day1;
-		month = month1; 
-		year = year1;
-		
-		System.out.printf("%d/%d/%d", day1, month1, year1);
-		
+
+	// Construtor passando parametros na classe data
+	Data(int day, int month, int year) {
+		// caso queiramos alterar nossa variavel de instancia de forma direta
+		// utilizamos o this
+
+		// this é utilizado para referenciar o objeto que está sendo criado neste
+		// momento
+		this.day = day;
+		this.month = month;
+		this.year = year;
 	}
-	//adicionando o método para mostrar a data
+
+	// adicionando o método para mostrar a data
 	String showDate() {
-		return String.format("%d/%d/%d ", day, month, year);
+		// devido ao método showDate pertencer a instancia por não ser static
+		// podemos acessar através do this, também
+
+		// métodos static não podem ser acessados pelo this
+		return String.format("%d/%d/%d ", this.day, this.month, this.year);
+	}
+	
+	void printFormatedDate() {
+		System.out.println(showDate());
 	}
 }
--- JavaUdemy/src/classe/DataTeste.java
+++ JavaUdemy/src/classe/DataTeste.java
@@ -6,13 +6,18 @@ public class DataTeste {
 		//Após a criação dos construtores, não será necessário criar as variaveis do objeto, pois 
 		//o parametro já foi passado.1'
 		Data date1 = new Data();
+		
 		Data date2 = new Data(28, 12, 2018);
-
-		// System.out.println(date1.day + "/" + date1.month + "/" + date1.year);
-
+		
+		String formatedDate1 = date1.showDate();
+		
 		// adicionando o método showDate para mostrar a data completa através da função
-		System.out.println("\n" + date1.showDate());
+		
+		System.out.println(formatedDate1);
+		System.out.println(date2.showDate());
 
+		date1.showDate();
+		date2.showDate();
 
 	}
 
