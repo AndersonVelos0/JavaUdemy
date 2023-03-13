f6dd19e3288fc838de55277e5e0bed35b9f789a2
Anderson <Anderson@DESKTOP-KAB4ON6>
2023-03-13 00:14:47 -0300

    Inserindo uma forma de acessar os valores criados fora do método main dentro da classe PrimeiroTrauma

---
+++ JavaUdemy/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,20 @@
+package classe;
+
+public class PrimeiroTrauma {
+	// como chamar essa variavel dentro do método main sem ter declarado-a dentro
+	// dele?
+	int a = 3;
+	static int b = 4;
+
+	public static void main(String[] args) {
+
+		// é necessário criarmos uma instancia para chamarmos o atributo
+
+		PrimeiroTrauma x = new PrimeiroTrauma();
+		System.out.println(x.a);
+
+		// outra forma de acessar é transformando o atributo em static
+		System.out.println(b);
+	}
+
+}
